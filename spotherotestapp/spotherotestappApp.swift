f5d4b9aa700c2779486f089523865f3a474db79d
Peter McGuire <petermcgu@gmail.com>
2021-08-22 14:37:06 -0700

    Initial Commit

---
+++ spotherotestapp/spotherotestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotherotestappApp.swift
+//  spotherotestapp
+//
+//  Created by Peter McGuire on 2021-08-22.
+//
+
+import SwiftUI
+
+@main
+struct spotherotestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
